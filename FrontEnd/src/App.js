0fd6e3cabc22bd1fa4f4c866dd40588dd1ba0002
namratapatil97 <patilnamrata221297@gmail.com>
2022-07-27 16:58:22 +0530

    Expense Tracker

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom'
+import Rounting from './components/Rounting';
+
+
+function App() {
+
+  return (
+
+    <>
+      <div className='allbgclr'>
+        <BrowserRouter>
+          <Rounting />
+        </BrowserRouter>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
